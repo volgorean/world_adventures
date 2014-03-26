9165f7019bada42547ffeb28d19f93230e49860b
pt <volgorean@gmail.com>
2014-03-25 23:36:42 -0700

    added functionality for making a post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,14 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     render :show_post
   end
+
+  def new
+    @post = Post.new
+  	render :new_post
+  end
+
+  def create
+    Post.create(params[:post])
+    redirect_to ''
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 WorldAdventures::Application.routes.draw do
   resources :posts
-  get 'post/:id', :to => 'posts#show'
   root 'posts#index'
+  get 'post/:id', :to => 'posts#show'
+  get '/new', :to => 'posts#new'
+
 end
